0a4952ebd6de294750f230f48d466f0d9c0c8350
Ayman Tarig <aymantarig17@gmail.com>
2025-02-01 22:11:41 +0200

    Created 'Love Story' by A2SV Hub

---
+++ 01-Feb-2025/Love Story 233955.py
@@ -0,0 +1,13 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+t = int(input())
+
+base = "codeforces"
+for _ in range(t):
+    s = input().strip()
+    count = 0
+    for i in range(10):
+        if s[i] != base[i]:
+            count += 1
+
+    print(count)
