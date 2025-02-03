e908b0ebfade6f5248257405a38b456359cec991
Ayman Tarig <aymantarig17@gmail.com>
2025-02-03 19:28:52 +0200

    Created 'Team' by A2SV Hub

---
+++ 03-Feb-2025/Team 237013.py
@@ -0,0 +1,10 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+problems = int(input())
+solve = 0
+for _ in range(problems):
+    ar = list(map(int, input().split()))
+    if sum(ar) >= 2:
+        solve += 1
+    
+print(solve)
