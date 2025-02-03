282985034442cc08a65aeace083aa44ad71751ca
Ayman Tarig <aymantarig17@gmail.com>
2025-02-03 19:29:16 +0200

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 03-Feb-2025/Way Too Long Words 237014.py
@@ -0,0 +1,10 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    if len(s) <= 10:
+        print(s)
+    else:
+        print(f"{s[0]}{len(s) - 2}{s[-1]}")
