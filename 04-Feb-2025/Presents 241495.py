0963ab5d03f65c57def8922af7d69ec47b79e50c
Ayman Tarig <aymantarig17@gmail.com>
2025-02-04 22:01:29 +0200

    Created 'Presents' by A2SV Hub

---
+++ 04-Feb-2025/Presents 241495.py
@@ -0,0 +1,9 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+n = int(input())
+ar = list(map(int, input().split()))
+
+res = [0] * n
+for i in range(n):
+    res[ar[i] - 1] = i + 1  
+print(*res)
