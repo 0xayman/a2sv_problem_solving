196b883d501d9155575f2ec919129b74bd777899
Ayman Tarig <aymantarig17@gmail.com>
2025-02-06 22:32:42 +0200

    Created 'Dragons' by A2SV Hub

---
+++ 06-Feb-2025/Dragons 248461.py
@@ -0,0 +1,17 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+strength, n = map(int, input().split())
+dragons = []
+for i in range(n):
+    dragons.append(list(map(int, input().split())))
+
+dragons.sort(key=lambda x: x[0])
+
+for i in range(n):
+    if strength > dragons[i][0]:
+        strength += dragons[i][1]
+    else:
+        print("NO")
+        break
+else:
+    print("YES")
