2f0d6dc9040296dabc09ce883f98d8f78f36ce75
Ayman Tarig <aymantarig17@gmail.com>
2025-02-22 22:46:12 +0200

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 22-Feb-2025/A. Beautiful Matrix 280614.py
@@ -0,0 +1,8 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+for i in range(5):
+    line = input().split()
+    for j in range(5):
+        if line[j] == "1":
+            distance = abs(j - 2) + abs(i - 2)
+print(distance)
